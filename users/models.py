a727c7100c4028ba88221ee5d6e4ef336e50b92e
Maxiel-Martinez <maxielmartinezc@gmail.com>
2024-04-25 15:21:27 -0500

    proyecto en proceso2

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class User(models.Model):
+    nombre = models.CharField(max_length=255)
+    telefono = models.CharField(max_length=15, blank=True, null=True)
+    correo = models.EmailField(unique=True)
+    cedula = models.CharField(max_length=20, unique=True)
+    firma = models.ImageField(upload_to='firmas/', null=True, blank=True)
