0c7a07520b0c113b4206555fb9b05a69f880b10e
Maxiel-Martinez <maxielmartinezc@gmail.com>
2024-05-20 11:01:23 -0500

    Arreglos html

--- retorno/settings.py
+++ retorno/settings.py
@@ -116,16 +116,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-<<<<<<< HEAD
 
 # Define las carpetas donde se encuentran los archivos estáticos
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-=======
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
->>>>>>> 571fc03284cb6b5e766f2f0a6324776c594e8123
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
