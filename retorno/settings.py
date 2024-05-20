5b28e50550531e403e2859cfb3becb80617589ec
Maxiel-Martinez <maxielmartinezc@gmail.com>
2024-05-19 19:23:28 -0500

    Cambios HTML

--- retorno/settings.py
+++ retorno/settings.py
@@ -114,7 +114,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Define las carpetas donde se encuentran los archivos estáticos
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
