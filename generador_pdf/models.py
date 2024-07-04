b50945541b9a46b3ed740d36551cb75c98550b64
HaroldCone <haroldconejo123@gmail.com>
2024-07-04 08:03:23 -0500

    Ya subi lo de la casa :3 Pendiente observar

--- generador_pdf/models.py
+++ generador_pdf/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
 
+# Get an instance of a FileExtensionValidator
+validate_pdf_extension = FileExtensionValidator(allowed_extensions=['pdf'])
 
+class Proyecto(models.Model):
+    # Define fields for your Proyecto model
+    ...
+
+class DevolucionEquipo(models.Model):
+    nombre = models.CharField(max_length=150, null=False, blank=False)
+    proyecto = models.ForeignKey(Proyecto, on_delete=models.CASCADE)
+    diadema = models.BooleanField(default=False)
+    camara = models.BooleanField(default=False)
+    raton = models.BooleanField(default=False)
+    teclado = models.BooleanField(default=False)
+    pantallas = models.PositiveIntegerField(default=1)
+    dispositivo_tipo = models.CharField(
+        max_length=15,
+        choices=[
+            ('torre', 'Torre'),
+            ('mini', 'Mini'),
+            ('portatil', 'Portatil')
+        ],
+        default='torre'
+    )
+    correo_personal = models.EmailField(null=False, blank=False)
+    correo_jefe = models.EmailField(null=False, blank=False)
+    firma_empleado = models.FileField(upload_to='firmas/', validators=[validate_pdf_extension])
+    firma_jefe = models.FileField(upload_to='firmas/', validators=[validate_pdf_extension])
+    firma_rrhh = models.FileField(upload_to='firmas/', validators=[validate_pdf_extension])
+
+    def __str__(self):
+        return f"Devolución de equipo {self.nombre} para {self.proyecto}"
